33c0d04f18978caa510b35c9a78da5f2bba8e9a5
Arifcse21 <arifcse21@gmail.com>
2023-06-05 22:06:28 +0600

    password field added on user schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -4,5 +4,6 @@ from pydantic import BaseModel, EmailStr
 class UserSchema(BaseModel):
     fullname: str | None = None
     email: EmailStr | None = None
+    password: str | None = None
     phone: str | None = None
     address: str | None = None
