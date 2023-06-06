9f564aa955afc1438b5640673f51db0466720e74
Arifcse21 <arifcse21@gmail.com>
2023-06-06 21:08:25 +0600

    create all engine

--- app/api.py
+++ app/api.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.routers import register_user_router
+from app.utils import Database
+from app.models import Base
 
+
+db = Database()
+engine, session = db()
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
 
