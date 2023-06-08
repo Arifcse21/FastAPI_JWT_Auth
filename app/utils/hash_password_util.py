b5a51b8ff6cbbbfec6d9e143830a5c306781ed32
Arifcse21 <arifcse21@gmail.com>
2023-06-08 11:39:44 +0600

    hasing password util added

---
+++ app/utils/hash_password_util.py
@@ -0,0 +1,9 @@
+import os, hashlib, binascii
+
+def hash_password(password):
+    salt = hashlib.sha256(os.urandom(64)).hexdigest().encode('ascii')
+    password_hash = hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),
+    salt, 100000)
+    password_hash = binascii.hexlify(password_hash)
+    return (salt+password_hash).decode('ascii')
+
