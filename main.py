464112d1c765e36f091fbfb70fde396b3285790c
Arifcse21 <arifcse21@gmail.com>
2023-06-10 19:02:32 +0600

    load .env properties

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+from dotenv import load_dotenv
 import uvicorn
 
 if __name__ == "__main__":
-    os.system("""export $( grep -vE "^(#.*|\s*)$" .env )""")    # load environment variable from .env file
+    load_dotenv()    # load environment variable from .env file
     uvicorn.run(app="app.api:app", host="127.0.0.1", port=8081, reload=True)
     
